732357149221aa4922c8188a7cd4ecb773ebe985
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-01-02 19:23:23 -0500

    树皮还原，望远镜mod

---
+++ kubejs/server_scripts/bark_interact.js
@@ -0,0 +1,73 @@
+// priority: 0
+
+const LOG_OR_WOOD = ["_log", "_wood"]
+const STEM_OR_HYPHAE = ["_stem", "_hyphae"]
+const VD_MAGIC_VINE = ["", "_block"]
+
+const FUNG_TYPE = ["minecraft:warped", "minecraft:crimson"]
+const TREE_TYPE = [
+    "minecraft:dark_oak",
+    "minecraft:oak",
+    "minecraft:acacia",
+    "minecraft:birch",
+    "minecraft:jungle",
+    "minecraft:spruce",
+    "minecraft:mangrove",
+    "minecraft:cherry",
+    "vintagedelight:magic_vine"
+]
+
+function applyTreeBark(event, namespace, tree, suffixes, bark) {
+    suffixes.forEach(suffix => {
+        let strippedId = `${namespace}:stripped_${tree}${suffix}`;
+        let originalId = `${namespace}:${tree}${suffix}`;
+        let block = event.block
+
+        if (block.id == strippedId && event.item.id == bark) {
+            let blockState = block.getBlockState();
+            let property0 = blockState.getProperties().toArray()[0]
+            let axis = blockState.getValue(property0).name().toLowerCase();
+            let level = event.level
+            let player = event.player
+
+            if (!player.isCreative()) {event.item.count--;}
+            event.server.runCommandSilent(`setblock ${block.x} ${block.y} ${block.z} ${originalId}[axis=${axis}]`)
+            // Play a sound effect
+            level.playSound(null, player.x, player.y, player.z, 'minecraft:item.bone_meal.use', 'ambient', 1.0, 1.0);
+            // Spawn particles
+            level.spawnParticles('minecraft:happy_villager', true, block.x + 0.5, block.y + 0.5, block.z + 0.5, 0.5, 0.5, 0.5, 10, 0.1);
+
+            // Cancel further processing
+            event.cancel();
+        }
+    });
+}
+function giveTreeBark(event, namespace, tree, suffixes) {
+    suffixes.forEach(suffix => {
+        let originalId = `${namespace}:${tree}${suffix}`;
+        
+        if (event.block.id == originalId && event.item.hasTag('minecraft:axes')) {
+            event.player.give('farmersdelight:tree_bark');
+        }
+    })
+}
+
+BlockEvents.rightClicked(event => {
+
+    TREE_TYPE.forEach(tree_type => {
+        let [namespace, tree] = tree_type.split(":");
+        if (event.block.id.contains(tree)) {
+            applyTreeBark(event, namespace, tree, LOG_OR_WOOD, 'farmersdelight:tree_bark');
+            applyTreeBark(event, namespace, tree, LOG_OR_WOOD, 'minecraft:bone_meal');
+            applyTreeBark(event, namespace, tree, VD_MAGIC_VINE, 'minecraft:bone_meal');
+
+            giveTreeBark(event, namespace, tree, LOG_OR_WOOD);
+        }
+    });
+    FUNG_TYPE.forEach(fung_type => {
+        let [namespace, fung] = fung_type.split(":");
+        if (event.block.id.contains(fung)) {
+            applyTreeBark(event, namespace, fung, STEM_OR_HYPHAE, 'minecraft:bone_meal');
+        }
+    });
+});
--- kubejs/server_scripts/dragon_breath.js
+++ kubejs/server_scripts/dragon_breath.js
@@ -6,21 +6,23 @@ console.info('Hello, World! (Loaded server scripts)')
 
 // Listen to player interact event
 BlockEvents.rightClicked(event => {
+    let block = event.block
     // Check if the block being right-clicked is a dragon head or a wall dragon head
-    if (event.block.id == 'minecraft:dragon_head' || event.block.id == 'minecraft:dragon_wall_head') {
-        // Check if the item in the player's hand is a bottle of builder's tea
-        if (event.item.id == 'create:builders_tea') {
-            // Give the player a bottle of dragon breath
-            event.player.give('minecraft:dragon_breath');
-            // Decrease the item stack of the empty bottle by 1
-            event.player.mainHandItem.count--;
-            // Play a sound effect
-            event.level.playSound(null, event.block.x, event.block.y, event.block.z, 'minecraft:entity.ender_dragon.ambient', 'ambient', 1.0, 1.0);
-            event.level.playSound(null, event.player.x, event.player.y, event.player.z, 'minecraft:item.bottle.fill_dragonbreath', 'ambient', 1.0, 1.0);
-            // Spawn particles
-            event.level.spawnParticles('minecraft:dragon_breath', true, event.block.x + 0.5, event.block.y + 0.5, event.block.z + 0.5, 0.1, 0.1, 0.1, 200, 0.1);
-            // Cancel the default event to prevent further processing
-            event.cancel();
-        }
+    if ( (block.id == 'minecraft:dragon_head' || block.id == 'minecraft:dragon_wall_head') &&
+        event.item.id == 'create:builders_tea' // Check if the item in the player's hand is a bottle of builder's tea
+    ) {
+        let player = event.player
+        let level = event.level
+        // Give the player a bottle of dragon breath
+        player.give('minecraft:dragon_breath');
+        // Decrease the item stack of the empty bottle by 1
+        if (!player.isCreative()) {item.count--};
+        // Play a sound effect
+        level.playSound(null, block.x, block.y, block.z, 'minecraft:entity.ender_dragon.ambient', 'ambient', 1.0, 1.0);
+        level.playSound(null, player.x, player.y, player.z, 'minecraft:item.bottle.fill_dragonbreath', 'ambient', 1.0, 1.0);
+        // Spawn particles
+        level.spawnParticles('minecraft:dragon_breath', true, block.x + 0.5, block.y + 0.5, block.z + 0.5, 0.1, 0.1, 0.1, 200, 0.1);
+        // Cancel the default event to prevent further processing
+        event.cancel();
     }
 });
