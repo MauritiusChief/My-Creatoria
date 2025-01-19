0598d4d58dbf43bb063ce857beb91a30ea358697
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-01-19 03:25:59 -0500

    创哥理赔

---
+++ kubejs/server_scripts/creative_bro.js
@@ -0,0 +1,13 @@
+
+ServerEvents.commandRegistry(event => {
+	const { commands: Commands, arguments: Arguments } = event
+
+	event.register(Commands.literal('cra-bro')
+		.executes(c => bank(c.source.player))
+	)
+	let bank=(player)=>{
+		player.getLevel().getServer().runCommandSilent("/give @p create:schedule")
+		player.setStatusMessage("创哥理赔完成");
+		return 1;
+	}
+})
