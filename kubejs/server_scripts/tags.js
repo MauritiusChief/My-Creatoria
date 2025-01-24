b043e17cdcbb54d95e637963497bfd34ed9c7218
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-01-24 02:08:54 -0500

    mod更新和tag

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,4 +1,5 @@
 
 ServerEvents.tags('item', event => {
     event.add('forge:seeds', ['vintagedelight:oat_seeds', 'vintagedelight:ghost_pepper_seeds', 'vintagedelight:cucumber_seeds'])
+    event.add('forge:plates/zinc', ['createdeco:zinc_sheet'])
 })
