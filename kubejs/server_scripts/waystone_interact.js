fcb831cfbc7c48c8d7cc82a293cb0f477b37bd96
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-02-08 16:36:12 -0500

    远程应力更新 part2 传送石碑配方更新 传送石碑手动摆放

---
+++ kubejs/server_scripts/waystone_interact.js
@@ -0,0 +1,33 @@
+
+const WAYSTONE_INGREDIENT = new Map([
+    ['waystones:waystone', ['minecraft:stone_bricks','minecraft:stone_bricks']],
+    ['waystones:sandy_waystone', ['minecraft:chiseled_sandstone','minecraft:chiseled_sandstone']],
+    ['waystones:mossy_waystone', ['minecraft:mossy_stone_bricks','minecraft:mossy_stone_bricks']],
+    ['waystones:sharestone', ['minecraft:chiseled_stone_bricks','minecraft:chiseled_stone_bricks']],
+    ['waystones:portstone', ['minecraft:stone_brick_stairs','minecraft:air']],
+])
+
+BlockEvents.rightClicked(event => {
+    
+    WAYSTONE_INGREDIENT.forEach((ingredient, waystone) => {
+        if (event.item.id == "waystones:warp_stone" && event.block.id == ingredient[0]) {
+            // console.log(event.item.id)
+            // console.log(event.block.id)
+            let block = event.block
+            if (
+                event.level.getBlock(block.x, block.y + 1, block.z).id != ingredient[1] && 
+                event.level.getBlock(block.x, block.y - 1, block.z).id != "minecraft:obsidian"
+            ) return;
+            if (!event.player.isCreative()) {event.item.count--;}
+            event.server.runCommandSilent(`setblock ${block.x} ${block.y + 1} ${block.z} ${waystone}[half=upper]`);
+            event.server.runCommandSilent(`setblock ${block.x} ${block.y} ${block.z} ${waystone}[half=lower]`);
+            
+            // Play a sound effect
+            event.level.playSound(null, block.x, block.y, block.z, 'minecraft:block.portal.travel', 'ambient', 0.1, 1.0);
+            // Spawn particles
+            event.level.spawnParticles('minecraft:portal', true, block.x + 0.5, block.y + 0.5, block.z + 0.5, 0.1, 0.2, 0.1, 20, 5.0);
+
+            event.cancel();
+        }
+    })
+})
