fae6a111351f1af1980853830c8f9710709ed8c7
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-02-08 18:26:15 -0500

    远程应力更新 part4 创哥理赔完成，删掉旧mod

--- kubejs/server_scripts/creative_bro.js
+++ kubejs/server_scripts/creative_bro.js
@@ -6,7 +6,8 @@ ServerEvents.commandRegistry(event => {
 		.executes(c => bank(c.source.player))
 	)
 	let bank=(player)=>{
-		// player.getLevel().getServer().runCommandSilent("/give @p create:schedule")
+		// player.getLevel().getServer().runCommandSilent("/give @p minecraft:beacon 3")
+		// player.getLevel().getServer().runCommandSilent("/give @p create:precision_mechanism 6")
 		player.setStatusMessage("创哥理赔完成");
 		return 1;
 	}
