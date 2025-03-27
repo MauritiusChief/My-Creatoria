e6f456e4be24aad3fc6e7be993f45fea970ea402
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-03-27 11:18:52 -0400

    Prepare to remove create deco

--- kubejs/server_scripts/creative_bro.js
+++ kubejs/server_scripts/creative_bro.js
@@ -2,17 +2,17 @@
 ServerEvents.commandRegistry(event => {
 	const { commands: Commands, arguments: Arguments } = event
 
-	event.register(Commands.literal('cra-bro')
+	event.register(Commands.literal('creative-bro')
 		.executes(c => bank(c.source.player))
 	)
 	let bank=(player)=>{
-		// player.getLevel().getServer().runCommandSilent("/give @p create:schedule")
+		player.getLevel().getServer().runCommandSilent("/give @p create:schedule")
 		
 		// player.getLevel().getServer().runCommandSilent("/give @p minecraft:beacon 3")
 		// player.getLevel().getServer().runCommandSilent("/give @p create:precision_mechanism 6")
 
 		// player.getLevel().getServer().runCommandSilent("/give @p waystones:warp_stone")
-		player.getLevel().getServer().runCommandSilent("/tp @e[type=wither] 0 -1000 0")
+		// player.getLevel().getServer().runCommandSilent("/tp @e[type=wither] 0 -1000 0")
 		player.setStatusMessage("创哥理赔完成");
 		return 1;
 	}
--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -3,3 +3,7 @@ ServerEvents.tags('item', event => {
     event.add('forge:seeds', ['vintagedelight:oat_seeds', 'vintagedelight:ghost_pepper_seeds', 'vintagedelight:cucumber_seeds'])
     event.add('forge:plates/zinc', ['createdeco:zinc_sheet'])
 })
+
+ServerEvents.tags('block', event => {
+    event.add('minecraft:mineable/pickaxe', ['createdeco:industrial_iron_bars', 'createdeco:industrial_iron_bars_overlay'])
+})
