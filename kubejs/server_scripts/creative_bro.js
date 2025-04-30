22160c3feed2cafa69ad19adb0aaa3ac81c21cb6
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2025-04-30 14:32:20 -0400

    准备更新机械动力6.0了

--- kubejs/server_scripts/creative_bro.js
+++ kubejs/server_scripts/creative_bro.js
@@ -3,17 +3,27 @@ ServerEvents.commandRegistry(event => {
 	const { commands: Commands, arguments: Arguments } = event
 
 	event.register(Commands.literal('creative-bro')
-		.executes(c => bank(c.source.player))
+		.executes(c => crtbro(c.source.player))
 	)
-	let bank=(player)=>{
-		player.getLevel().getServer().runCommandSilent("/give @p create:schedule")
-		
-		// player.getLevel().getServer().runCommandSilent("/give @p minecraft:beacon 3")
-		// player.getLevel().getServer().runCommandSilent("/give @p create:precision_mechanism 6")
-
-		// player.getLevel().getServer().runCommandSilent("/give @p waystones:warp_stone")
-		// player.getLevel().getServer().runCommandSilent("/tp @e[type=wither] 0 -1000 0")
-		player.setStatusMessage("创哥理赔完成");
-		return 1;
-	}
 })
+
+let crtbro = (player) => {
+	let server = player.getLevel().getServer()
+	// server.runCommandSilent("/give @p create:schedule")
+	// server.runCommandSilent(`/give @p create:andesite_alloy ${(8+4)/4+16/8+9*2+20+63}`)
+	// server.runCommandSilent(`/give @p minecraft:iron_ingot ${(64*2+5+36+60+24)/4+14+8/2}`)
+	// server.runCommandSilent(`/give @p minecraft:iron_ingot ${(60)/4+64+20+64+57}`)
+	// server.runCommandSilent(`/give @p create:andesite_alloy ${(64+3)*2}`)
+	// server.runCommandSilent(`/give @p minecraft:torch ${(64+3)}`)
+	// server.runCommandSilent(`/give @p minecraft:iron_ingot ${(3)*2}`)
+	// server.runCommandSilent(`/give @p minecraft:torch ${(3)}`)
+	// server.runCommandSilent(`/give @p create:zinc_ingot ${16}`)
+	
+	// server.runCommandSilent("/give @p minecraft:beacon 3")
+	// server.runCommandSilent("/give @p create:precision_mechanism 6")
+
+	// server.runCommandSilent("/give @p waystones:warp_stone")
+	// server.runCommandSilent("/tp @e[type=wither] 0 -1000 0")
+	player.setStatusMessage("创哥理赔完成");
+	return 1;
+}
--- kubejs/server_scripts/dragon_breath.js
+++ kubejs/server_scripts/dragon_breath.js
@@ -6,13 +6,13 @@ console.info('Hello, World! (Loaded server scripts)')
 
 // Listen to player interact event
 BlockEvents.rightClicked(event => {
-    let block = event.block
+    const block = event.block
+    const player = event.player
+    const level = event.level
     // Check if the block being right-clicked is a dragon head or a wall dragon head
     if ( (block.id == 'minecraft:dragon_head' || block.id == 'minecraft:dragon_wall_head') &&
         event.item.id == 'create:builders_tea' // Check if the item in the player's hand is a bottle of builder's tea
     ) {
-        let player = event.player
-        let level = event.level
         // Give the player a bottle of dragon breath
         player.give('minecraft:dragon_breath');
         // Decrease the item stack of the empty bottle by 1
@@ -23,6 +23,6 @@ BlockEvents.rightClicked(event => {
         // Spawn particles
         level.spawnParticles('minecraft:dragon_breath', true, block.x + 0.5, block.y + 0.5, block.z + 0.5, 0.1, 0.1, 0.1, 200, 0.1);
         // Cancel the default event to prevent further processing
-        event.cancel();
+        event.success();
     }
 });
